d7d4b126c4f3f7503289e0ea4beecfde14e69187
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-09-03 14:26:12 +0700

    chore: homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)}>
         <Toaster
           toastOptions={{
             duration: 1000,
