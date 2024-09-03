7d5a146969aed06dfd05b02a852c1f6687820e52
Thanh Thanh <thanhthanh186203@gmail.com>
2024-09-03 13:50:03 +0700

    feat: design table in account source --v1

---
+++ src/app/dashboard/account-source/data-table.tsx
@@ -0,0 +1,166 @@
+'use client'
+
+import {
+  ColumnDef,
+  SortingState,
+  flexRender,
+  VisibilityState,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  getPaginationRowModel,
+  ColumnFiltersState,
+  useReactTable
+} from '@tanstack/react-table'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import React from 'react'
+import {
+  BadgePlus,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  ChevronsUpDown,
+  CirclePlus
+} from 'lucide-react'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection
+    }
+  })
+
+  return (
+    <div>
+      <Card className='px-5 py-3'>
+        <div className='flex items-center justify-between'>
+          <div className='justify-betweenyả flex items-center py-2'>
+            <Input
+              placeholder='Filter emails...'
+              value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
+              onChange={(event) => table.getColumn('email')?.setFilterValue(event.target.value)}
+              className='max-w-lg'
+            />
+            <Button variant='default' className='ml-2'>
+              <CirclePlus className='pr-1' />
+              Status
+            </Button>
+            <Button variant='default' className='ml-2'>
+              <CirclePlus className='pr-1' />
+              Priority
+            </Button>
+          </div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='default' className='hover:#306e58 ml-auto'>
+                  View
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                {table
+                  .getAllColumns()
+                  .filter((column) => column.getCanHide())
+                  .map((column) => {
+                    return (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className='capitalize'
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                      >
+                        {column.id}
+                      </DropdownMenuCheckboxItem>
+                    )
+                  })}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+        <div className='rounded-md border'>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
+                    )
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                    ))}
+                    <TableCell className='text-center font-bold'>...</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={columns.length} className='h-24 text-center'>
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className='flex items-center justify-between space-x-2'>
+          <p className='text-gray-500'>0 of 100 row(s) selected.</p>
+          <div className='flex items-center justify-end space-x-2 py-4'>
+            <p>Rows per page</p>
+            <Button variant='outline' className='mr-10 border-gray-300'>
+              10 <ChevronsUpDown size={15} className='ml-2 text-gray-600' />
+            </Button>
+            <p>Page 1 of 10</p>
+            <Button className='ml-5' variant='outline' size='sm' onClick={() => table.previousPage()}>
+              <ChevronLeft />
+            </Button>
+            <Button variant='outline' size='sm' onClick={() => table.nextPage()}>
+              <ChevronRight />
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  )
+}
--- src/app/dashboard/account-source/page.tsx
+++ src/app/dashboard/account-source/page.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import { Payment, columns } from './columns'
+import { DataTable } from './data-table'
 
-export default function page() {
-  return <div>account source</div>
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      name: 'abc',
+      amount: 3555,
+      initAmount: 3333,
+      currentAmount: 3233,
+      userId: 'we1',
+      status: 'pending'
+    }
+    // ...
+  ]
+}
+
+export default async function page() {
+  const data = await getData()
+
+  return (
+    <div className='mx-auto'>
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
 }
