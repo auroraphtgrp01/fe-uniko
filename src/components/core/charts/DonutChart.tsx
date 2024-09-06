6fc097689dafd1600e464ce4f296c4d0087e6dcd
Trần Nguyễn Duy Khánh <147808163+AugustusDngQt@users.noreply.github.com>
2024-09-06 22:07:59 +0700

    fix: fix bug of donut charts

--- src/components/core/charts/DonutChart.tsx
+++ src/components/core/charts/DonutChart.tsx
@@ -18,80 +18,79 @@ export interface IPayloadDataChart {
 const DonutChart = ({ options, data, className, types = 'donut' }: IChartProps) => {
   const chartRef = useRef(null)
 
-  const chartOptions = {
-    donut: {
-      tooltip: { trigger: 'item' },
-      legend: { top: '5%', left: 'center' },
-      series: [
-        {
-          name: 'Access From',
-          type: 'pie',
-          radius: ['20%', '70%'],
-          avoidLabelOverlap: false,
-          itemStyle: {
-            borderRadius: 10,
-            borderColor: '#fff',
-            borderWidth: 5
-          },
-          label: {
-            fontSize: '10'
-          },
-          data
-        }
-      ]
-    },
-    nightingale: {
-      legend: { top: 'bottom' },
-      toolbox: {
-        show: true,
-        feature: {
-          mark: { show: true },
-          dataView: { show: true, readOnly: false },
-          restore: { show: true },
-          saveAsImage: { show: true }
-        }
-      },
-      tooltip: { trigger: 'item' },
-      series: [
-        {
-          name: 'Nightingale Chart',
-          type: 'pie',
-          radius: [50, 150],
-          center: ['50%', '50%'],
-          roseType: 'area',
-          itemStyle: {
-            borderRadius: 10
-          },
-          data
-        }
-      ]
-    },
-    referer: {
-      tooltip: { trigger: 'item' },
-      legend: {
-        orient: 'vertical',
-        left: 'bottom'
-      },
-      series: [
-        {
-          name: 'Access From',
-          type: 'pie',
-          radius: '80%',
-          data,
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
-    }
-  }
-
   useEffect(() => {
     const chartInstance = init(chartRef.current)
+    const chartOptions = {
+      donut: {
+        tooltip: { trigger: 'item' },
+        legend: { top: '5%', left: 'center' },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['20%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 5
+            },
+            label: {
+              fontSize: '10'
+            },
+            data
+          }
+        ]
+      },
+      nightingale: {
+        legend: { top: 'bottom' },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        tooltip: { trigger: 'item' },
+        series: [
+          {
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: [50, 150],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            itemStyle: {
+              borderRadius: 10
+            },
+            data
+          }
+        ]
+      },
+      referer: {
+        tooltip: { trigger: 'item' },
+        legend: {
+          orient: 'vertical',
+          left: 'bottom'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '80%',
+            data,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+    }
     chartInstance.setOption(chartOptions[types])
     return () => {
       chartInstance.dispose()
