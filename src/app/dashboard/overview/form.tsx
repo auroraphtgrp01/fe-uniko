341eea38901031fa51a8ff8d190ff7d8481bba39
Thanh Thanh <thanhthanh186203@gmail.com>
2024-09-07 19:39:00 +0700

    feat: responsive layout overview

--- src/app/dashboard/overview/form.tsx
+++ src/app/dashboard/overview/form.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import CardInHeader from '@/components/dashboard/CardInHeader'
 
 export default function DashboardMainForm() {
-  return <div className='container mx-auto px-4 py-4'></div>
+  return (
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
+      <div className='w-full md:col-span-1'>
+        <div className='grid grid-cols-1 gap-4'>
+          <CardInHeader className='w-full' />
+          <CardInHeader className='w-full' />
+        </div>
+      </div>
+      <div className='w-full md:col-span-2'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+          <CardInHeader className='w-full' />
+          <CardInHeader className='w-full' />
+        </div>
+        <div className='mt-4'>
+          <CardInHeader className='w-full' />
+        </div>
+      </div>
+    </div>
+  )
 }
