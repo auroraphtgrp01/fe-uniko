e80256b8a635711901c6de6b01da35c8e882e4d4
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-09-07 16:42:25 +0700

    fix: nprogres

--- src/components/core/top-progress-bar.ts
+++ src/components/core/top-progress-bar.ts
@@ -1,7 +1,8 @@
 'use client'
+import { useEffect } from 'react'
 import Router from 'next/router'
 import NProgress from 'nprogress'
-
+import 'nprogress/nprogress.css'
 let timer: NodeJS.Timeout | undefined
 let state: 'loading' | 'stop' | undefined
 let activeRequests = 0
@@ -15,6 +16,12 @@ function load() {
   state = 'loading'
 
   timer = setTimeout(() => {
+    NProgress.configure({
+      showSpinner: true,
+      minimum: 0.1,
+      easing: 'ease',
+      speed: 800
+    })
     NProgress.start()
   }, delay)
 }
@@ -32,31 +39,41 @@ function stop() {
   NProgress.done()
 }
 
-Router.events.on('routeChangeStart', load)
-Router.events.on('routeChangeComplete', stop)
-Router.events.on('routeChangeError', stop)
-
-const originalFetch = window.fetch
-window.fetch = async function (...args: Parameters<typeof originalFetch>) {
-  if (activeRequests === 0) {
-    load()
-  }
-
-  activeRequests++
-
-  try {
-    const response = await originalFetch(...args)
-    return response
-  } catch (error) {
-    return Promise.reject(error)
-  } finally {
-    activeRequests -= 1
+function handleFetch() {
+  const originalFetch = window.fetch
+  window.fetch = async function (...args: Parameters<typeof originalFetch>) {
     if (activeRequests === 0) {
-      stop()
+      load()
+    }
+
+    activeRequests++
+
+    try {
+      const response = await originalFetch(...args)
+      return response
+    } catch (error) {
+      return Promise.reject(error)
+    } finally {
+      activeRequests -= 1
+      if (activeRequests === 0) {
+        stop()
+      }
     }
   }
 }
 
 export default function NProgressHandler() {
+  useEffect(() => {
+    Router.events.on('routeChangeStart', load)
+    Router.events.on('routeChangeComplete', stop)
+    Router.events.on('routeChangeError', stop)
+    handleFetch()
+    return () => {
+      Router.events.off('routeChangeStart', load)
+      Router.events.off('routeChangeComplete', stop)
+      Router.events.off('routeChangeError', stop)
+    }
+  }, [])
+
   return null
 }
