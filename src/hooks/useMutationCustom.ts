a643272a1ba494704f2e2decad631ced3507d082
auroraphtgrp01 <sliverknight4869@gmail.com>
2024-09-23 11:03:49 +0700

    chore: docstring for hook

--- src/hooks/useMutationCustom.ts
+++ src/hooks/useMutationCustom.ts
@@ -18,6 +18,24 @@ export interface IUseMutationCustomProps<TData, TError, TVariables> {
 
 const baseUrl = getBaseUrl()
 
+/**
+ * Custom React hook to perform mutations using the `useMutation` hook from `@tanstack/react-query`.
+ * This hook integrates with a custom `postData` function and allows for additional query parameters,
+ * headers, and other mutation options.
+ *
+ * @template TBody - The type of the request body.
+ * @template TResponse - The type of the response.
+ *
+ * @param {IUseMutationCustomProps<TResponse, AxiosError, TBody>} props - The properties for configuring the mutation.
+ * @param {string} props.pathUrl - The path URL to append to the base URL for the mutation endpoint.
+ * @param {QueryOptions} [props.options] - Optional query parameters, conditions, and headers.
+ * @param {IDynamicType} [props.options.params] - Optional query parameters to include in the request URL.
+ * @param {string|null} [props.options.condition] - Optional condition to include in the request (not used in this implementation).
+ * @param {Record<string, string>} [props.options.headers] - Optional headers to include in the request.
+ * @param {UseMutationOptions<TResponse, AxiosError, TBody>} [props.mutateOption] - Options to customize the behavior of `useMutation`.
+ *
+ * @returns {ReturnType<typeof useMutation>} - The return value of `useMutation` with custom configuration.
+ */
 export const useMutationCustom = <TBody, TResponse>({
   pathUrl,
   options = {},
