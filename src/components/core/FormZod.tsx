7adeaff91ff87e73effec854f2c6e8fbb6eecab9
vtrong <trongdn2405@gmail.com>
2024-10-28 16:24:10 +0700

    feat: add form zod

--- src/components/core/FormZod.tsx
+++ src/components/core/FormZod.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -40,6 +40,10 @@ export default function FormZod<T extends z.ZodRawShape>({
     onSubmit(value)
   }
 
+  useEffect(() => {
+    form.reset(defaultValues)
+  }, [defaultValues, form])
+
   return (
     <div>
       <Form {...form}>
@@ -49,82 +53,90 @@ export default function FormZod<T extends z.ZodRawShape>({
               control={form.control}
               name={fieldItem.name as any}
               key={index}
-              render={({ field }) => (
-                <FormItem>
-                  {fieldItem?.label && <FormLabel className='text-muted-foreground'>{fieldItem.label}</FormLabel>}
-                  <FormControl>
-                    <>
-                      {fieldItem.type === EFieldType.Input && (
-                        <Input
-                          placeholder={fieldItem.placeHolder}
-                          {...(fieldItem.props as InputProps)}
-                          {...field}
-                          value={String(field.value ?? '')}
-                        />
-                      )}
-                      {fieldItem.type === EFieldType.Select && (
-                        <Select
-                          onValueChange={(value) => {
-                            field.onChange(value)
-                          }}
-                        >
-                          <SelectTrigger className={cn('w-full', fieldItem?.classNameTrigger)}>
-                            <SelectValue placeholder={fieldItem.placeHolder} />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectGroup>
-                              <SelectLabel>Select {fieldItem.label}</SelectLabel>
-                              {fieldItem?.dataSelector?.map((item: IDynamicType) => (
-                                <SelectItem key={item.value} value={item.value}>
-                                  {item.label}
-                                </SelectItem>
-                              ))}
-                            </SelectGroup>
-                          </SelectContent>
-                        </Select>
-                      )}
-                      {fieldItem.type === EFieldType.Textarea && (
-                        <Textarea
-                          {...field}
-                          placeholder={fieldItem.placeHolder}
-                          {...(fieldItem.props as TextareaProps)}
-                          value={String(field.value ?? '')}
-                        />
-                      )}
+              render={({ field }) => {
+                return (
+                  <FormItem className={(fieldItem.props as any).className}>
+                    {fieldItem?.label && <FormLabel className='text-muted-foreground'>{fieldItem.label}</FormLabel>}
+                    <FormControl>
+                      <>
+                        {fieldItem.type === EFieldType.Input && (
+                          <Input
+                            placeholder={fieldItem.placeHolder}
+                            {...(fieldItem.props as InputProps)}
+                            {...field}
+                            onChange={(value) => {
+                              field.onChange(value)
+                            }}
+                          />
+                        )}
+                        {fieldItem.type === EFieldType.Select && (
+                          <Select
+                            onValueChange={(value) => {
+                              field.onChange(value)
+                            }}
+                            {...(fieldItem.props as any)}
+                            {...field}
+                          >
+                            <SelectTrigger className={cn('w-full', fieldItem?.classNameTrigger)}>
+                              <SelectValue placeholder={fieldItem.placeHolder} />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectLabel>Select {fieldItem.label}</SelectLabel>
+                                {fieldItem?.dataSelector?.map((item: IDynamicType) => (
+                                  <SelectItem key={item.value} value={item.value}>
+                                    {item.label}
+                                  </SelectItem>
+                                ))}
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        )}
+                        {fieldItem.type === EFieldType.Textarea && (
+                          <Textarea
+                            {...field}
+                            placeholder={fieldItem.placeHolder}
+                            {...(fieldItem.props as TextareaProps)}
+                            onChange={(value) => {
+                              field.onChange(value)
+                            }}
+                          />
+                        )}
 
-                      {fieldItem.type === EFieldType.Combobox && (
-                        <Combobox
-                          {...(fieldItem.props as IComboboxProps)}
-                          onValueSelect={(value) => {
-                            field.onChange(value)
-                            console.log(`${fieldItem.name} selected:`, value)
-                          }}
-                        />
-                      )}
-                      {fieldItem.type === EFieldType.DatePicker && (
-                        <DateTimePicker
-                          {...field}
-                          {...(fieldItem.props as DateTimePickerProps)}
-                          onChange={(value) => {
-                            field.onChange(value)
-                          }}
-                        />
-                      )}
-                      {fieldItem.type === EFieldType.DateRangePicker && (
-                        <DateRangePicker
-                          {...field}
-                          {...(fieldItem.props as DateRangePickerProps)}
-                          value={field.value as DateRange | undefined}
-                          onChange={(value) => {
-                            field.onChange(value)
-                          }}
-                        />
-                      )}
-                    </>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+                        {fieldItem.type === EFieldType.Combobox && (
+                          <Combobox
+                            {...field}
+                            {...(fieldItem.props as IComboboxProps)}
+                            onValueSelect={(value) => {
+                              field.onChange(value)
+                            }}
+                          />
+                        )}
+                        {fieldItem.type === EFieldType.DatePicker && (
+                          <DateTimePicker
+                            {...field}
+                            {...(fieldItem.props as DateTimePickerProps)}
+                            onChange={(value) => {
+                              field.onChange(value)
+                            }}
+                          />
+                        )}
+                        {fieldItem.type === EFieldType.DateRangePicker && (
+                          <DateRangePicker
+                            {...field}
+                            {...(fieldItem.props as DateRangePickerProps)}
+                            value={field.value as DateRange | undefined}
+                            onChange={(value) => {
+                              field.onChange(value)
+                            }}
+                          />
+                        )}
+                      </>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }}
             />
           ))}
           <Button {...buttonConfig} type='submit'>
